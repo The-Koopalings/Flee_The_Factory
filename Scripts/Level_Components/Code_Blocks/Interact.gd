38ce2249fd171b6b4bc2c10d67dc2f54dd6bfc92
Henry Chen <henrychennj@gmail.com>
2024-02-07 00:57:36 -0500

    Undid path changes b/c relative paths aren't working for some reason

--- Scripts/Level_Components/Code_Blocks/Interact.gd
+++ Scripts/Level_Components/Code_Blocks/Interact.gd
@@ -3,9 +3,9 @@ extends Area2D
 signal interactSignal
 
 func _ready():
-	var Robot = get_node("../../Grid/Robot")
+	var Robot = get_node("/root/ProofOfConcept/Grid/Robot")
 	connect("interactSignal",Robot,"_on_Interact_interactSignal")
-	
+	pass
 	
 
 func _process(delta):
--- Scripts/Level_Components/Code_Blocks/Rotate_Right.gd
+++ Scripts/Level_Components/Code_Blocks/Rotate_Right.gd
@@ -3,7 +3,7 @@ extends Area2D
 signal rotateRightSignal
 
 func _ready():
-	var Robot = get_node("../../Grid/Robot")
+	var Robot = get_node("/root/ProofOfConcept/Grid/Robot")
 	connect("rotateRightSignal",Robot,"_on_RotateRight_rotateRightSignal")
 	
 
