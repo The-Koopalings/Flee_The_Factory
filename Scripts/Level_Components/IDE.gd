8c561c049e0aa506a226bba9302bcf4ce22d3063
m-lin1 <43522378+m-lin1@users.noreply.github.com>
2024-02-15 15:18:08 -0500

    Calling F1 or F2 inside of F1 or F2 works properly, pause between end of functions and start of another is just right. Some repeat code in IDE.gd since putting yield statements into another function seems to not work correctly

--- Scripts/Level_Components/IDE.gd
+++ Scripts/Level_Components/IDE.gd
@@ -1,12 +1,14 @@
 extends VBoxContainer
 
-signal functionFinished
+signal function1Finished
+signal function2Finished
 
 #Entry point for IDE code
 onready var main = get_node("Main/FunctionBlockArea")
 onready var f1 = get_node("F1/FunctionBlockArea")
 onready var f2 = get_node("F2/FunctionBlockArea")
-var callFunction = false
+var regexF1 = RegEx.new()
+var regexF2 = RegEx.new()
 
 func _ready():
 	#Spacing between function blocks
@@ -14,6 +16,10 @@ func _ready():
 	
 	#Moves Run_Button to the bottom
 	move_child(get_child(1), get_child_count() - 1)
+	
+	#Regex for F1 and F2 code blocks
+	regexF1.compile("F1_")
+	regexF2.compile("F2_")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +29,7 @@ func _ready():
 #Connected to Run_Button
 func _on_Button_pressed():
 	var code = main.get_children()
+	
 	#Pop all the non-code nodes {CollisionShape2D, ColorRect}
 	code.pop_front()
 	code.pop_front()
@@ -32,25 +39,21 @@ func _on_Button_pressed():
 	
 	#Run all of the code + add delay between each block
 	for block in code:
-		block.send_signal()
-		if callFunction:
-			yield(self, "functionFinished")
-		yield(get_tree().create_timer(GameStats.run_speed, false), "timeout") 
-	#run_code(code)
-	#get_tree().create_timer(GameStats.run_speed, false)
+		if regexF1.search(block.name):
+			block.send_signal()
+			yield(self, "function1Finished")
+		elif regexF2.search(block.name):
+			block.send_signal()
+			yield(self, "function2Finished")
+		else:
+			block.send_signal()
+			yield(get_tree().create_timer(GameStats.run_speed, false), "timeout") 
 
 
+#Run F1 code
 func _on_f1Signal():
 	var code = f1.get_children()
-	run_func_code(code)
 	
-	
-func _on_f2Signal():
-	var code = f2.get_children()
-	run_func_code(code)
-	
-func run_func_code(code):
-	callFunction = true
 	#Pop all the non-code nodes {CollisionShape2D, ColorRect}
 	code.pop_front()
 	code.pop_front()
@@ -60,9 +63,63 @@ func run_func_code(code):
 	
 	#Run all of the code + add delay between each block
 	for block in code:
-		block.send_signal()
-		yield(get_tree().create_timer(GameStats.run_speed, false), "timeout") 
+		if regexF1.search(block.name):
+			block.send_signal()
+			yield(self, "function1Finished")
+		elif regexF2.search(block.name):
+			block.send_signal()
+			yield(self, "function2Finished")
+		else:
+			block.send_signal()
+			yield(get_tree().create_timer(GameStats.run_speed, false), "timeout") 
+	emit_signal("function1Finished")
 	
-	callFunction = false
-	emit_signal("functionFinished")
+	
+#Run F2 code
+func _on_f2Signal():
+	var code = f2.get_children()
+	
+	#Pop all the non-code nodes {CollisionShape2D, ColorRect}
+	code.pop_front()
+	code.pop_front()
+	
+	#debug so we know what's running
+	print(code)
+	
+	#Run all of the code + add delay between each block
+	for block in code:
+		if regexF1.search(block.name):
+			block.send_signal()
+			yield(self, "function1Finished")
+		elif regexF2.search(block.name):
+			block.send_signal()
+			yield(self, "function2Finished")
+		else:
+			block.send_signal()
+			yield(get_tree().create_timer(GameStats.run_speed, false), "timeout") 
+	emit_signal("function2Finished")
+	
+	
+#Helper function to run F1 & F2 code
+#func run_func_code(code):
+#	#Pop all the non-code nodes {CollisionShape2D, ColorRect}
+#	code.pop_front()
+#	code.pop_front()
+#
+#	#debug so we know what's running
+#	print(code)
+#
+#	#Run all of the code + add delay between each block
+#	for block in code:
+#		if regexF1.search(block.name):
+#			block.send_signal()
+#			yield(self, "function1Finished")
+#		elif regexF2.search(block.name):
+#			block.send_signal()
+#			yield(self, "function2Finished")
+#		else:
+#			block.send_signal()
+#		yield(get_tree().create_timer(GameStats.run_speed, false), "timeout") 
+#
+
 		
