09b43061765534ee54f6b597132cfaeb1c5a70a2
m-lin1 <43522378+m-lin1@users.noreply.github.com>
2024-02-22 18:39:05 -0500

    Added Key item scene

--- Scripts/Level_Components/Inventory.gd
+++ Scripts/Level_Components/Inventory.gd
@@ -9,14 +9,20 @@ func _ready():
 			$ReferenceRect.set_border_color(Color(1, 0, 0)) #Red
 			$Line2D.set_default_color(Color(1, 0, 0))
 			$Line2D2.set_default_color(Color(1, 0, 0))
+			$Line2D3.set_default_color(Color(1, 0, 0))
+			$Line2D4.set_default_color(Color(1, 0, 0))
 		1:
 			$ReferenceRect.set_border_color(Color(0, 1, 0)) #Green
 			$Line2D.set_default_color(Color(0, 1, 0))
 			$Line2D2.set_default_color(Color(0, 1, 0))
+			$Line2D3.set_default_color(Color(0, 1, 0))
+			$Line2D4.set_default_color(Color(0, 1, 0))
 		2:
 			$ReferenceRect.set_border_color(Color(0, 0, 1)) #Blue
 			$Line2D.set_default_color(Color(0, 0, 1))
 			$Line2D2.set_default_color(Color(0, 0, 1))
+			$Line2D3.set_default_color(Color(0, 0, 1))
+			$Line2D4.set_default_color(Color(0, 0, 1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
