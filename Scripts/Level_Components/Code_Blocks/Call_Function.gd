195035f89ead95f362b89b448b47de7273482d2f
m-lin1 <43522378+m-lin1@users.noreply.github.com>
2024-02-22 16:24:54 -0500

    initial CallControlFlow code block + IDE section. Might change to separate if-else block and if-else IDE section later

--- Scripts/Level_Components/Code_Blocks/Call_Function.gd
+++ Scripts/Level_Components/Code_Blocks/Call_Function.gd
@@ -3,24 +3,27 @@ extends Area2D
 signal f1Signal
 signal f2Signal
 
+var resultF1
+var resultF2
+
 func _ready():
 	var IDE = get_node("../../../../IDE")
 	connect("f1Signal", IDE, "_on_f1Signal")
 	connect("f2Signal", IDE, "_on_f2Signal")
-
 	
-func _process(delta):
-	pass
-
-
-func send_signal():
+	#Get the regex ready for use in send_signal()
 	var regexF1 = RegEx.new()
 	var regexF2 = RegEx.new()
 	regexF1.compile("F1_")
 	regexF2.compile("F2_")
-	var resultF1 = regexF1.search(name)
-	var resultF2 = regexF2.search(name) 
+	resultF1 = regexF1.search(name)
+	resultF2 = regexF2.search(name) 
 	
+#func _process(delta):
+#	pass
+
+
+func send_signal():
 	if resultF1:
 		print("CALLING FUNCTION1")
 		emit_signal("f1Signal")
