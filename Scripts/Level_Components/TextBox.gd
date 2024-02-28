10b99159ed7c046382e93dced1c515678e6623cf
helarious99 <helenahe@ufl.edu>
2024-02-27 21:55:16 -0500

    Added the ability for text box to change positions

--- Scripts/Level_Components/TextBox.gd
+++ Scripts/Level_Components/TextBox.gd
@@ -11,7 +11,7 @@ onready var text = $TextBoxContainer/MarginContainer/HBoxContainer/Text
 enum State {
 	READY,
 	READING,
-	END
+	END,
 }
 
 var current_state = State.READY
@@ -25,19 +25,32 @@ func _ready():
 func _process(delta):
 	match current_state:
 		State.READY:
-			# Display text from queue
 			if !text_queue.empty():
-				display_text()
+				# Change position before displaying text if needed
+				if text_queue[0] == "IDE":
+					text_queue.pop_front()
+					set_IDE_pos()
+				elif text_queue[0] == "CODE_BLOCK":
+					text_queue.pop_front()
+					set_codeblock_pos()
+				elif text_queue[0] == "DEFAULT":
+					text_queue.pop_front()
+					set_default_pos()
+				# Display text from queue
+				else:
+					display_text()
+					change_state(State.READING)
 		State.READING:
 			# Press enter to display all text right away
 			if Input.is_action_just_pressed("ui_accept"):
 				text.percent_visible = 1.0
-				$Tween.stop_all()
+				$Tween.remove_all()
 				end_symbol.text = "v"
 				change_state(State.END)
 		State.END:
 			# Press enter to hide textbox
 			if Input.is_action_just_pressed("ui_accept"):
+				end_symbol.text = ""
 				if text_queue.empty():
 					hide_textbox()
 				change_state(State.READY)
@@ -62,10 +75,9 @@ func show_textbox():
 func display_text():
 	# Get next text from the queue
 	var dialogue = text_queue.pop_front()
+	
 	text.text = dialogue
 	text.percent_visible = 0.0
-	
-	change_state(State.READING)
 	show_textbox()
 	
 	# Animate text so it doesn't all appear at once
@@ -80,3 +92,18 @@ func _on_Tween_tween_all_completed():
 
 func change_state(state):
 	current_state = state
+
+
+func set_default_pos():
+	textbox_container.rect_position = Vector2(150, 740)
+	textbox_container.rect_size = Vector2(1620, 300)
+
+
+func set_codeblock_pos():
+	textbox_container.rect_position = Vector2(300, 600)
+	textbox_container.rect_size = Vector2(1010, 300)
+
+
+func set_IDE_pos():
+	textbox_container.rect_position = Vector2(300, 100)
+	textbox_container.rect_size = Vector2(1010, 300)
