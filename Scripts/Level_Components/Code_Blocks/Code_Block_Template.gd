a801aea93a8104fdab125960ab23d561e17f05a6
Henry Chen <henrychennj@gmail.com>
2024-03-04 01:19:51 -0500

    Double click to remove blocks + fixed bugginess in double click behavior.

--- Scripts/Level_Components/Code_Blocks/Code_Block_Template.gd
+++ Scripts/Level_Components/Code_Blocks/Code_Block_Template.gd
@@ -3,35 +3,44 @@ extends Area2D
 var dragging = false
 var parent
 var startPos
-var inFBA = false
 
 signal drag(area)
 signal stopDrag(globalPos)
-signal doubleClick(codeBlock)
+signal doubleClick(code_block)
 
 func _ready():
-	connect("stopDrag", get_node("../.."), "stop_drag") #connects to FunctionBlockArea that CodeBlock is grandchild of
+	
+	$Highlight.visible = false
+	
+	connect("stopDrag", get_node("../.."), "_on_CodeBlock_stop_drag") #connects to FunctionBlockArea that CodeBlock is grandchild of
 	parent = self.get_parent()
 	startPos = parent.global_position
 	
+	var temp = get_parent()
+	var path = "../"
+	while !temp.has_node("IDE"):
+		temp = temp.get_parent()
+		path += "../"
+	var IDE = get_node(path + "IDE")
+	
 	# Connect double click signal to all function blocks
-	var Main = get_node("../../../IDE/Main/FunctionBlockArea")
+	var Main = IDE.get_node("Main/FunctionBlockArea")
 	connect("doubleClick", Main, "_on_CodeBlock_doubleClick")
 	
-	var F1 = get_node("../../../IDE/F1/FunctionBlockArea")
+	var F1 = IDE.get_node("F1/FunctionBlockArea")
 	connect("doubleClick", F1, "_on_CodeBlock_doubleClick")
 	
-	var F2 = get_node("../../../IDE/F2/FunctionBlockArea")
+	var F2 = IDE.get_node("F2/FunctionBlockArea")
 	connect("doubleClick", F2, "_on_CodeBlock_doubleClick")
 	
-	var If1 = get_node("../../../IDE/IfElse1/If/FunctionBlockArea")
+	var If1 = IDE.get_node("IfElse1/If/FunctionBlockArea")
 	connect("doubleClick", If1, "_on_CodeBlock_doubleClick")
-	var Else1 = get_node("../../../IDE/IfElse1/Else/FunctionBlockArea")
+	var Else1 = IDE.get_node("IfElse1/Else/FunctionBlockArea")
 	connect("doubleClick", Else1, "_on_CodeBlock_doubleClick")
 	
-	var If2 = get_node("../../../IDE/IfElse2/If/FunctionBlockArea")
+	var If2 = IDE.get_node("IfElse2/If/FunctionBlockArea")
 	connect("doubleClick", If2, "_on_CodeBlock_doubleClick")
-	var Else2 = get_node("../../../IDE/IfElse2/Else/FunctionBlockArea")
+	var Else2 = IDE.get_node("IfElse2/Else/FunctionBlockArea")
 	connect("doubleClick", Else2, "_on_CodeBlock_doubleClick")
 	
 	
@@ -48,15 +57,14 @@ func _process(delta):
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
-			dragging=!dragging
-				
-			if not event.pressed:
+			if event.doubleclick:
+				var code_block = get_parent()
+				emit_signal("doubleClick", code_block)
+			elif event.pressed:
+				dragging = true
+			elif !event.pressed:
+				dragging = false
 				emit_signal("stopDrag", startPos)
-				
-		if event.doubleclick:
-			var code_block = get_parent()
-			emit_signal("doubleClick", code_block)
-			
 
 func _on_CodeBlock_mouse_entered():
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
