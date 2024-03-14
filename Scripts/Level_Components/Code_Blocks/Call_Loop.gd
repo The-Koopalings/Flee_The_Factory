bf5248bea7b90f755f62d902457e0bc1e6dd10c8
Henry Chen <henrychennj@gmail.com>
2024-03-14 02:21:22 -0400

    Sprite for loop block changed to be able to handle any number of loops

--- Scripts/Level_Components/Code_Blocks/Call_Loop.gd
+++ Scripts/Level_Components/Code_Blocks/Call_Loop.gd
@@ -5,7 +5,7 @@ const BLOCK_TYPE = "CALL"
 #Which loop type is chosen
 var type = "none" 
 var loopBlockName = ""
-
+var loopBlockNumber = ""
 func _ready():
 	connect_to_LoopBlock()
 
@@ -19,6 +19,8 @@ func connect_to_LoopBlock():
 	else:
 		loopBlockName = name.trim_prefix("Call_").rstrip("0123456789").trim_suffix("_")
 	
+	loopBlockNumber = loopBlockName.trim_prefix("Loop")
+
 	var loopBlockNode = get_node(PEP.get_path_to_grandpibling(self, "IDE/" + loopBlockName)) 
 	status = loopBlockNode.connect("ChosenLoop", self, "on_loop_type_selected")
 	if status != 0:
@@ -34,18 +36,10 @@ func connect_to_LoopBlock():
 #type is either For or While (anything else does nothing)
 func on_loop_type_selected(type: String):
 	self.type = type
-	#Loop1
-	if loopBlockName == "Loop1":
-		if type == "While":
-			$Sprite.texture = load("res://Assets/Objects/While1.png")
-		elif type == "For":
-			$Sprite.texture = load("res://Assets/Objects/For1.png")
-	#Loop2
-	elif loopBlockName == "Loop2":
-		if type == "While":
-			$Sprite.texture = load("res://Assets/Objects/While2.png")
-		elif type == "For":
-			$Sprite.texture = load("res://Assets/Objects/For2.png")
+	 
+	#Example path: "res://Assets/Objects/While1.png"
+	$Sprite.texture = load("res://Assets/Objects/" + type + loopBlockNumber + ".png")
+	
 
 
 func send_signal():
