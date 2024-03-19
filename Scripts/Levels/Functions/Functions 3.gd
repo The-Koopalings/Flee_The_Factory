6ba335d0474ead6d2ada104cfcc98b1230f17c20
helarious99 <helenahe@ufl.edu>
2024-03-19 19:35:53 -0400

    Added lvl 3 + dialogue

--- Scripts/Levels/Functions/Functions 3.gd
+++ Scripts/Levels/Functions/Functions 3.gd
@@ -4,8 +4,9 @@ extends Node2D
 onready var Grid = get_node("Grid")
 onready var CodeBlockBar = get_node("CodeBlockBar")
 onready var MainFBA = get_node("IDE/Main/FunctionBlockArea")
-#onready var TextBox = get_node("TextBox")
+onready var TextBox = get_node("TextBox")
 signal levelComplete
+var level_win = false
 ##UNIVERSAL LEVEL VARIABLES 
 
 ##UNIQUE LEVEL VARIABLES
@@ -13,35 +14,54 @@ var b1_pressed = false
 var b2_pressed = false
 var b3_pressed = false
 
-#signal dialogue_progress
-#var progress_check = [false, false]
-#var progress_check_arr = [["Forward"], ["Forward", "Forward", "Forward", "Interact"]]
+signal dialogue_progress
+var progress_check = [false, false]
+var progress_check_arr = [["Forward", "Forward", "Forward", "Interact"], ["RotateRight", "Forward", "Forward", "RotateLeft"]]
+onready var progress_check_FBA = [$IDE/F1/FunctionBlockArea, $IDE/F2/FunctionBlockArea]
 ##UNIQUE LEVEL VARIABLES
 
 ##LEVEL CONFIGURATION VARIABLES
 var tiles = [
 	['X','X','X','X','X','X','X','X','X','X','X'],
-	['X','X','X','R','O',' ',' ',' ','X','X','X'],
-	['X','X','X',' ','O','B','O',' ','X','X','X'],
-	['X','X','X',' ','O',' ','O',' ','X','X','X'],
-	['X','X','X','B','O',' ','O','B','X','X','X'],
-	['X','X','X',' ',' ',' ','O','D','X','X','X'],
+	['R',' ',' ','B','X','X','X','X','X','X','X'],
+	['X','X','X',' ','X','X','X','X','X','X','X'],
+	['X','X','X',' ',' ',' ','B','X','X','X','X'],
+	['X','X','X','X','X','X',' ','X','X','X','X'],
+	['X','X','X','X','X','X',' ',' ',' ','B','D'],
 	['X','X','X','X','X','X','X','X','X','X','X'],
 ]
-var robotStartOrientation = PEP.Orientation.DOWN
+var robotStartOrientation = PEP.Orientation.RIGHT
 ##LEVEL CONFIGURATION VARIABLES
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PEP.loadLevel(self)
+	DialogueManager.add_dialogue(self, "Functions/Functions 3.txt")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	DialogueManager.dialogue_progress_check(self)
+	
 	if b1_pressed and b2_pressed and b3_pressed:
-		emit_signal("levelComplete")
-		$AcceptDialog.popup()
-		b1_pressed = false
-		b2_pressed = false
-		b3_pressed = false
+		emit_signal("dialogue_progress")
+		
+		if level_win:
+			emit_signal("levelComplete")
+			$AcceptDialog.popup()
+			level_win = false
+
+
+#Handles all button presses
+func _on_Button_buttonPressed(name):
+	match name:
+		"Button1":
+			b1_pressed = true
+		"Button2":
+			b2_pressed = true
+		"Button3":
+			b3_pressed = true
+	
+	if b1_pressed and b2_pressed and b3_pressed:
+		level_win = true
