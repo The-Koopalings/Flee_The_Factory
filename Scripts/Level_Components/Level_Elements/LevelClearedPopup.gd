acacd863eecae998f56e476af343b77f3098a4de
helarious99 <helenahe@ufl.edu>
2024-04-03 17:31:08 -0400

    Next level button for level popup is functional

--- Scripts/Level_Components/Level_Elements/LevelClearedPopup.gd
+++ Scripts/Level_Components/Level_Elements/LevelClearedPopup.gd
@@ -1,9 +1,7 @@
 extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var level = get_node("../")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,7 +15,10 @@ func _ready():
 
 
 func _on_Next_Level_pressed():
-	pass # Replace with function body.
+	var root = get_tree().root
+	var directory = root.get_child(root.get_child_count() - 1).filename.get_base_dir()
+	
+	SceneSwapper.change_to_next_level_scene(directory + "/" + level.name + ".tscn")
 
 
 func _on_Exit_Level_pressed():
