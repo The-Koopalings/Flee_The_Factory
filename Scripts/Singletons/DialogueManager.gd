9a67ce0d904b277595f48d4be32517b95d95569b
m-lin1 <43522378+m-lin1@users.noreply.github.com>
2024-04-17 20:21:45 -0400

    Added highlight paths for Virus and Clear Code button in DialogueManager.gd

--- Scripts/Singletons/DialogueManager.gd
+++ Scripts/Singletons/DialogueManager.gd
@@ -28,10 +28,12 @@ var highlight_path = {"HIGHLIGHT_IDE": "IDE/IDE_Arrow",
 					  "HIGHLIGHT_DOOR": "Grid/Door/Highlight",
 					  "HIGHLIGHT_DOORR": "Grid/DoorR/Highlight",
 					  "HIGHLIGHT_OBSTACLE": "Grid/Obstacle/Highlight",
+					  "HIGHLIGHT_VIRUS": "Grid/Virus/Highlight",
 					  "HIGHLIGHT_SETTINGS": "Level Control/Settings/Arrow",
 					  "HIGHLIGHT_RESTART": "Level Control/Restart/Arrow",
 					  "HIGHLIGHT_HELP": "Level Control/Help/Arrow",
-					  "HIGHLIGHT_2X_SPEED": "Level Control/DoubleSpeed/Arrow"}
+					  "HIGHLIGHT_2X_SPEED": "Level Control/DoubleSpeed/Arrow",
+					  "HIGHLIGHT_CLEAR_CODE": "IDE/Clear_Code_Arrow"}
 
 var check_progress = false  # Boolean check to fix yielding bug
 var check_index = 0  # Only check for currently yielding user action checkpoint
