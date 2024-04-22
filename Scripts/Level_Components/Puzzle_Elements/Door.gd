047b34ba8d000b2f9a8459cad3da1124ebda3b96
Karen <liu.karen011@gmail.com>
2024-04-22 16:29:28 -0400

    HOLD DOOR

--- Scripts/Level_Components/Puzzle_Elements/Door.gd
+++ Scripts/Level_Components/Puzzle_Elements/Door.gd
@@ -52,5 +52,3 @@ func _on_level_levelComplete():
 		if IDE.is_loop() and IDE.code.size() == 0:
 			IDE.currentNode.increment_loopCount()
 	
-		#Rescale since placeholder texture is too big (b/c the door texture is tiny and needs to be scaled
-		$Sprite.set_scale(Vector2(1,1)) 
